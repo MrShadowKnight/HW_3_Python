2090cbd56a63da063bc22efd3812f1b5c34c5bc6
MrShadowKnight <138589809+MrShadowKnight@users.noreply.github.com>
2023-07-08 21:40:59 +0300

    Add.main

---
+++ main.py
@@ -0,0 +1,112 @@
+# TASK №1
+
+# number_week = 0
+# while number_week >= 1 or number_week <=7:
+#     number_week = int(input("Введіть номер тижню: "))
+#     print(" ")
+#     if number_week == 1:
+#         print("Понеділок.")
+#         break
+#     elif number_week == 2:
+#         print("Вівторок.")
+#         break
+#     elif number_week == 3:
+#         print("Середа.")
+#         break
+#     elif number_week == 4:
+#         print("Четвер.")
+#         break
+#     elif number_week == 5:
+#         print("П'ятниця.")
+#         break
+#     elif number_week == 6:
+#         print("Субота.")
+#         break
+#     elif number_week == 7:
+#         print("Неділя.")
+#         break
+#     else:
+#         print("Не існує такого дня тиждня.\n")
+
+# TASK №2
+
+# number_month = 0
+# while number_month >= 1 or number_month <=12:
+#     number_month = int(input("Введіть номер місяця: "))
+#     print(" ")
+#     if number_month == 1:
+#         print("Січень.")
+#         break
+#     elif number_month == 2:
+#         print("Лютий.")
+#         break
+#     elif number_month == 3:
+#         print("Березень.")
+#         break
+#     elif number_month == 4:
+#         print("Квітень.")
+#         break
+#     elif number_month == 5:
+#         print("Травень.")
+#         break
+#     elif number_month == 6:
+#         print("Червень.")
+#         break
+#     elif number_month == 7:
+#         print("Ливень.")
+#         break
+#     elif number_month == 8:
+#         print("Серпень.")
+#         break
+#     elif number_month == 9:
+#         print("Вересень.")
+#         break
+#     elif number_month == 10:
+#         print("Жовтень.")
+#         break
+#     elif number_month == 11:
+#         print("Листопад.")
+#         break
+#     elif number_month == 12:
+#         print("Грудень.")
+#         break
+#     else:
+#         print("Не існує такого місяця.\n")
+
+# TASK №3
+
+# number = int(input("Enter a number: "))
+# if number > 0:
+#     print("Number is positive")
+# elif number < 0:
+#     print("Number is negative")
+# elif number == 0:
+#     print("Number is equal to zero")
+# else:
+#     print("WHAT IS THE NUMBER?")
+
+# TASK №4
+
+# number_one = int(input("Please enter the first number: "))
+# number_two = int(input("Please enter the second number: "))
+# if number_one == number_two:
+#     print("These numbers are equal to")
+# elif number_one > number_two:
+#     print(number_one, " ", number_two)
+# elif number_two > number_one:
+#     print(number_two, " ", number_one)
+# else:
+#     print("I don't know what happened to these numbers!")
+
+# TASK SHOP
+
+products = {
+    'яблука': 10,
+    'банани': 15,
+    'апельсини': 12,
+    'груші': 8,
+    'киві': 20
+}
+
+balance = 100
+shopping_cart = []
